fb9770ba8aee8256c948fb25aeba624b63f03f5d
Marina Malygina <marina0391@mail.ru>
2022-04-25 10:56:45 +0300

    добавлено try-with-resources

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,22 +7,24 @@ import java.net.Socket;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         int port = 8090;
 
         while (true) {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept(); // ждем подключения
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept(); // ждем подключения
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-            System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
+                System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
 
-            String name = in.readLine();
-            out.println(String.format("Hi, %s, your port is %d", name, clientSocket.getPort()));
-            serverSocket.close();
+                String name = in.readLine();
+                out.println(String.format("Hi, %s, your port is %d", name, clientSocket.getPort()));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
     }
 }
